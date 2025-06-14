4b2dacbbbbad833eb2cad073b6328c748a257ea9
mayur wagh <mswagh98@gmail.com>
2025-06-14 12:00:20 +0530

    add dummy data and type for it

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface Provider {
+  id: string;
+  name: string;
+  specialization: string;
+  location: string;
+  rating: number;
+  shortDescription: string;
+  longDescription: string;
+  contactEmail: string;
+  phoneNumber: string;
+}
